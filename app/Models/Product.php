9089e4834a6197357598262fd26d7658c3a8f8ba
David Tahim <davidtahim@gmail.com>
2025-04-29 21:02:07 -0300

    final do banco de dados e eloquente

---
+++ app/Models/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /**
+* PRODUCT ATTRIBUTES
+* $this->attributes['id'] - int - contains the product primary key (id)
+* $this->attributes['name'] - string - contains the product name
+* $this->attributes['description'] - string - contains the product description
+* $this->attributes['image'] - string - contains the product image
+* $this->attributes['price'] - int - contains the product price
+* $this->attributes['created_at'] - timestamp - contains the product creation date
+* $this->attributes['updated_at'] - timestamp - contains the product update date
+*/
+
+}
